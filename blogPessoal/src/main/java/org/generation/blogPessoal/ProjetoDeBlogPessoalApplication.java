2b45d21ea59753a35d90204bc9bdd72846e07d8a
Giovanna Becker <gigibecker.cg3@gmail.com>
2022-08-02 21:25:15 -0300

    Criando repositório do Blog Pessoal

---
+++ blogPessoal/src/main/java/org/generation/blogPessoal/ProjetoDeBlogPessoalApplication.java
@@ -0,0 +1,13 @@
+package org.generation.blogPessoal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoDeBlogPessoalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoDeBlogPessoalApplication.class, args);
+	}
+
+}
