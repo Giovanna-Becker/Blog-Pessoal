04e8c226de41febaecca57c9e19dc247c20b518a
Giovanna Becker <gigibecker.cg3@gmail.com>
2022-08-19 22:25:58 -0300

    Deploy do projeto Blog Pessoal

--- blogPessoal/src/main/java/org/generation/blogPessoal/model/UsuarioModel.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/model/UsuarioModel.java
@@ -15,6 +15,8 @@ import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 @Entity
 @Table(name = "tb_usuario")
 public class UsuarioModel {
@@ -26,6 +28,7 @@ public class UsuarioModel {
 	@NotNull(message = "O atributo nome é obrigatório!")
 	private String nome;
 	
+	@Schema(example = "email@email.com.br")
 	@NotNull(message = "O atributo usuario é obrigatório!")
 	@Email(message = "O Atributo Usuário deve ser um email válido!")
 	private String usuario;
